680c2c7e45c3c1094ce4a9be70a7f851918d2722
Jonathan Norris <jonathan.s.norris5.civ@us.navy.mil>
2026-01-27 09:24:27 -0800

    added module root for mm2t

---
+++ src/mm2t.rs
@@ -0,0 +1,29 @@
+//! # lib_mm2t
+//!
+//! Provides an abstraction layer for interacting with the 900 MHz
+//! serial radio in a Tokio async contexts.
+//!
+//! This library exposes:
+//! - [`PacketT`]: the trait defining a packet to be sent as bytes over the MM2T port
+//! - [`MM2TTransport`]: a real hardware implementation using a serial port.
+
+// internal package imports
+pub(crate) mod checksum;
+pub(crate) mod models;
+
+// packets always included regardless of features
+mod packet;
+mod transport;
+
+
+// optional mm2t-rx feature
+#[cfg(feature = "mm2t-rx")]
+pub mod packet_decoder;
+
+// re-export commmon types and functions
+pub use transport::MM2TTransport;
+pub use packet::PacketT;
+#[cfg(feature = "mm2t-rx")]
+pub use packet_decoder::PacketDecoder;
+#[cfg(feature = "mm2t-rx")]
+pub use models::DecodedPacket;
